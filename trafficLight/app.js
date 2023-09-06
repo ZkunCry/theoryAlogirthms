c689e357ac5ddbcf8eaff4357cc588c7f00fed58
ZkunCry <aksentev.03@mail.ru>
2023-09-06 23:34:43 +0500

    finished work

--- trafficLight/app.js
+++ trafficLight/app.js
@@ -1,3 +1,7 @@
+const redPeople = document.querySelector(".redPeople");
+const greenPeople = document.querySelector(".greenPeople");
+const stateColumns = document.querySelectorAll(".state-column");
+let counter = 0;
 class State {
   constructor(name, duration, next, prev) {
     this.name = name;
@@ -5,6 +9,12 @@ class State {
     this.next = next;
     this.prev = prev;
   }
+}
+function Timer() {
+  let timerColumn = stateColumns[1];
+
+  timerColumn.innerText = `Timer:${counter}`;
+  ++counter;
 }
 
 class Controller {
@@ -18,23 +28,56 @@ class Controller {
 function toggleActive(stateElement) {
   const colorItem = document.querySelector(`.${stateElement.name}`);
   const currentItem = document.querySelector(`.${stateElement.prev.name}`);
-  colorItem.classList.add("active");
-  currentItem.classList.remove("active");
+  const prevPrevitem = document.querySelector(
+    `.${stateElement.prev.prev.name}`
+  );
+  stateColumns[0].innerText = `State:${stateElement.state}`;
+  if (stateElement.next.name === "yellowG") {
+    colorItem.classList.add("active");
+    currentItem.classList.remove("active");
+    greenPeople.classList.add("active");
+    redPeople.classList.remove("active");
+  } else if (stateElement.next.name === "green") {
+    colorItem.classList.add("active");
+  } else if (stateElement.prev.name === "yellowG") {
+    colorItem.classList.add("active");
+    currentItem.classList.remove("active");
+    prevPrevitem.classList.remove("active");
+    redPeople.classList.add("active");
+    greenPeople.classList.remove("active");
+  } else {
+    colorItem.classList.add("active");
+    currentItem.classList.remove("active");
+  }
 }
 let red, yellow, yellowForward, green;
-red = new State("red", 3);
-yellow = new State("yellow", 1);
-yellowForward = new State("yellow", 1);
-green = new State("green", 3);
+red = new State("red", 12);
+yellow = new State("yellowG", 2);
+yellowForward = new State("yellow", 2);
+green = new State("green", 10);
 
 red.next = yellow;
 red.prev = yellowForward;
+
 yellow.next = green;
 yellow.prev = red;
 green.next = yellowForward;
 green.prev = yellow;
 yellowForward.next = red;
 yellowForward.prev = green;
-
+red.state = "Q1";
+yellow.state = "Q2";
+green.state = "Q3";
+yellowForward.state = "Q4";
 const controller = new Controller();
+
+setInterval(() => {
+  if (counter === 24) {
+    setTimeout(() => {
+      counter = 0;
+      Timer();
+    }, 1000);
+  } else Timer();
+}, 1000);
+
 controller.trigger(red, toggleActive);
